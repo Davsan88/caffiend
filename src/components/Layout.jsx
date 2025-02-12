f91f0ecb71add16eb4eb5a0375834ec23b6c2f0c
Dav <david.sanchez.martinez@outlook.com>
2025-02-12 20:41:19 +0000

    Add Layout component with header, footer, and children

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,21 @@
 const Layout = ({ children }) => {
-  return (
-    <>
-      
-    </>
-  )
+    const header = (
+        <header></header>
+    )
+
+    const footer = (
+        <footer></footer>
+    )
+
+    return (
+        <>
+            {header}
+            <main>
+                {children}
+            </main>
+            {footer}
+        </>
+    )
 }
 
 export default Layout
