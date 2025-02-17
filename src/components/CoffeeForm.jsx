9579aa3672e038b5bdb7dee74afda3e70fedbc1c
Dav <david.sanchez.martinez@outlook.com>
2025-02-17 20:53:41 +0000

    Add button-based coffee options to CoffeeForm component

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,7 +1,23 @@
+import { coffeeOptions } from "../utils"
+
 const CoffeeForm = () => {
     return (
       <>
-        
+        <div className="section-header">
+          <i className="fa-solid fa-pencil" />
+          <h2>Start Tracking Today</h2>
+        </div>
+        <h4>Select coffee type</h4>
+        <div className="coffee-grid">
+          {coffeeOptions.slice(0, 5).map((option, optionIndex) => {
+            return (
+              <button className="button-card" key={optionIndex}> 
+                <h4>{option.name}</h4>
+                <p>{option.caffeine}mg</p>
+              </button>
+            )
+          })}
+        </div>
       </>
     )
   }
