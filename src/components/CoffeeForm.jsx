97fcf0ae7d66b12181a72d725242f3ccc9025cb3
Dav <david.sanchez.martinez@outlook.com>
2025-02-17 21:28:08 +0000

    Add 'Other' fallback button and select dropdown for coffee choices

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,26 +1,41 @@
 import { coffeeOptions } from "../utils"
 
 const CoffeeForm = () => {
-    return (
-      <>
-        <div className="section-header">
-          <i className="fa-solid fa-pencil" />
-          <h2>Start Tracking Today</h2>
-        </div>
-        <h4>Select coffee type</h4>
-        <div className="coffee-grid">
-          {coffeeOptions.slice(0, 5).map((option, optionIndex) => {
+  return (
+    <>
+      <div className="section-header">
+        <i className="fa-solid fa-pencil" />
+        <h2>Start Tracking Today</h2>
+      </div>
+      <h4>Select coffee type</h4>
+      <div className="coffee-grid">
+        {coffeeOptions.slice(0, 5).map((option, optionIndex) => {
+          return (
+            <button className="button-card" key={optionIndex}>
+              <h4>{option.name}</h4>
+              <p>{option.caffeine}mg</p>
+            </button>
+          )
+        })}
+        <button className="button-card">
+          <h4>Other</h4>
+          <p>n/a</p>
+        </button>
+        <select name="coffee-list" id="coffee-list">
+          <option value={null}>Select fuel</option>
+          {coffeeOptions.map((option, optionIndex) => {
             return (
-              <button className="button-card" key={optionIndex}> 
-                <h4>{option.name}</h4>
-                <p>{option.caffeine}mg</p>
-              </button>
+              <option value={option} key={optionIndex}>
+                {option.name} ({option.caffeine}mg)
+
+              </option>
             )
           })}
-        </div>
-      </>
-    )
-  }
-  
-  export default CoffeeForm
-  
+        </select>
+
+      </div>
+    </>
+  )
+}
+
+export default CoffeeForm
