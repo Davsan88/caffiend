c98d2e2f7018e71954f18b1def7360c891dfd860
Dav <david.sanchez.martinez@outlook.com>
2025-02-19 19:47:49 +0000

    Add cost and time selection inputs to form

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -21,18 +21,44 @@ const CoffeeForm = () => {
           <h4>Other</h4>
           <p>n/a</p>
         </button>
-        <select name="coffee-list" id="coffee-list">
-          <option value={null}>Select fuel</option>
-          {coffeeOptions.map((option, optionIndex) => {
-            return (
-              <option value={option} key={optionIndex}>
-                {option.name} ({option.caffeine}mg)
-
-              </option>
-            )
-          })}
-        </select>
-
+      </div>
+      <select name="coffee-list" id="coffee-list">
+        <option value={null}>Select fuel</option>
+        {coffeeOptions.map((option, optionIndex) => {
+          return (
+            <option value={option} key={optionIndex}>
+              {option.name} ({option.caffeine}mg)
+            </option>
+          )
+        })}
+      </select>
+      <h4>Add the cost (£)</h4>
+      <input className="w-full" type="number" placeholder="£4.20" />
+      <h4>Time since consumption</h4>
+      <div className="time-entry">
+        <div>
+          <h6>Hours</h6>
+          <select id="hour-select">
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map((hour, hourIndex) => {
+              return (
+                <option key={hourIndex} value={hour}>{hour}</option>
+              )
+            })}
+          </select>
+        </div>
+        <div>
+          <h6>Mins</h6>
+          <select id="mins-select">
+            {[0, 5, 10, 15, 30, 45].map((min, minIndex) => {
+              return (
+                <option key={minIndex} value={min}>{min}</option>
+              )
+            })}
+          </select>
+        </div>
+        <button>
+          Add Entry
+        </button>
       </div>
     </>
   )
