2f3b423785a3ac5de347fc52728513db0ea238fb
Dav <david.sanchez.martinez@outlook.com>
2025-03-23 20:04:33 +0000

    Add initial layout structure for Stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,13 @@
 const Stats = () => {
     return (
       <>
-        
+        <div className="section-header">
+          <i className="fa-solid fa-chart-simple" />
+          <h2>Stats</h2>  
+        </div>  
+        <div className="stats-grid">
+
+        </div>
       </>
     )
   }
