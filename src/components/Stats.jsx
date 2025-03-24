cefb06ab74ea454d3704d9e9a72876c451661be9
Dav <david.sanchez.martinez@outlook.com>
2025-03-24 21:07:57 +0000

    Add StatCard layout and titles to Stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -7,20 +7,21 @@ const StatCard = ({ lg, title, children }) => {
   )
 }
 
+  return (
+    <>
+      <div className="section-header">
+        <i className="fa-solid fa-chart-simple" />
+        <h2>Stats</h2>
+      </div>
+      <div className="stats-grid">
+        <StatCard lg title="Active Caffeine Level"></StatCard>
+        <StatCard title="Daily Caffeine"></StatCard>
+        <StatCard title="Average # of Coffees"></StatCard>
+        <StatCard title="Daily Cost (£)"></StatCard>
+        <StatCard title="Total Cost (£)"></StatCard>
+      </div>
+    </>
+  )
+}
 
-const Stats = () => {
-    return (
-      <>
-        <div className="section-header">
-          <i className="fa-solid fa-chart-simple" />
-          <h2>Stats</h2>  
-        </div>  
-        <div className="stats-grid">
-
-        </div>
-      </>
-    )
-  }
-  
-  export default Stats
-  
+export default Stats
